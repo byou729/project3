0de106463e5614297c7ce45b1b7bbfcf07fc282c
Mohammed Chawla <chawla.mohammed@gmail.com>
2018-10-06 03:50:11 -0500

    added react router and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Chicken from "./components/pages/Chicken";
+import Beef from "./components/pages/Beef";
+import Sides from "./components/pages/Sides";
+import Dessert from "./components/pages/Dessert";
 //import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+      <Router>
+      <div>
+        <Navbar />
+        <Route exact path="/" component={Chicken} />
+        <Route exact path="/beef" component={Beef} />
+        <Route exact path="/sides" component={Sides} />
+        <Route exact path="/dessert" component={Dessert} />
+      </div>
+    </Router>
       </div>
     );
   }
