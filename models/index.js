f85e28c5e0af0461377947207daaff0e4f248e4f
Ben You <byou729@gmail.com>
2018-10-15 19:19:17 -0500

    working app

--- models/index.js
+++ models/index.js
@@ -1,11 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
   Category: require("./category"),
   MenuItem: require("./menuItem"),
 };
-=======
-    Category: require("./category"),
-    MenuItem: require("./menuItem"),
-    User: require("./user")
-  };
->>>>>>> 795b5906f2c1e3d29736d15d7d416c16e16f296d
